f2340a03b94b9666b8e60b84fb1d07b029167be5
S-Hiro <hiroto-_-1610@docomo.ne.jp>
2021-02-26 18:54:48 +0900

    クリアシーン、クリアカットシーン追加

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] string clearScene = "";
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadSceneAsync(clearScene);
+        }
+    }
+}
