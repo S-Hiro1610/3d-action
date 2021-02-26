f790f53a42480076979556b272e75ef6526f93a7
S-Hiro <hiroto-_-1610@docomo.ne.jp>
2021-02-26 18:14:41 +0900

    鍵のオブジェクトを追加

---
+++ Assets/Scripts/KeyController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyController : MonoBehaviour
+{
+    [SerializeField] AudioClip m_se = null;
+    AudioSource m_audio;
+    private void Start()
+    {
+        m_audio = GetComponent<AudioSource>();
+    }
+    // Player が接触したら音を鳴らしてこのオブジェクトを消す
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            m_audio.PlayOneShot(m_se);
+            Destroy(this.gameObject);
+        }
+    }
+}
